c2ec3f8a1d6f9b08f2efa82e3c4f8a1b92c1c413
Ricardo Olea <testsun905@gmail.com>
2022-09-26 22:51:31 -0500

    refactor vc

--- SignBarsTest/Controller/ViewController.swift
+++ SignBarsTest/Controller/ViewController.swift
@@ -61,54 +61,6 @@ class ViewController: UIViewController {
         
     }
     
-    func JSONWT(str: String?){
-        let parts = str?.components(separatedBy: ".")
-        if parts?.count != 3 { fatalError("jwt is not valid!") }
-        //Token
-        let header = parts?[0]
-        print("tokenPart1\(header)")
-        defaults.set(header, forKey: "firstPartToken")
-        //Contain info User
-        let payload = parts?[1]
-        let signature = parts?[2]
-        print(decodeJWTPart(part: payload ?? "") ?? "could not converted to json!")
-        let name  = decodeJWTPart(part: payload ?? "")
-        //print(name)
-        //print(name?.keys)
-        if let fbemail = (name as AnyObject)["titular"]! as? String
-        {
-            //Save in userdefaults
-            defaults.set(fbemail, forKey: "Usuario")
-            print(defaults.string(forKey: "Usuario"))
-        }
-    }
-    
-    func base64StringWithPadding(encodedString: String) -> String {
-        var stringTobeEncoded = encodedString.replacingOccurrences(of: "-", with: "+")
-            .replacingOccurrences(of: "_", with: "/")
-        let paddingCount = encodedString.count % 4
-        for _ in 0..<paddingCount {
-            stringTobeEncoded += "="
-        }
-        return stringTobeEncoded
-    }
-    
-    func decodeJWTPart(part: String) -> [String: Any]? {
-        let payloadPaddingString = base64StringWithPadding(encodedString: part)
-        guard let payloadData = Data(base64Encoded: payloadPaddingString) else {
-            fatalError("payload could not converted to data")
-        }
-        return try? JSONSerialization.jsonObject(
-            with: payloadData,
-            options: []) as? [String: Any]
-    }
-    
-    func alertView(){
-        let alert = UIAlertController(title: "Alert", message: "Email o contraseña incorrecto", preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
 }
 
 
